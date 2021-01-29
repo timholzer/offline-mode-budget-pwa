4560db3ddc311d88de322736ba509e03d9a314dd
Tim Holzer <timiholzer@gmail.com>
2021-01-28 20:57:16 -0700

    added menifest and service worker

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+
+const FILES_TO_CACHE = [
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/styles.css", 
+    "/icons/icon-144x144.png",
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png"
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
